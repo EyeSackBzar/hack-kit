362afed8b22d0aaf42fed92b5742fde7ff9f7a45
isaac b r <isaacbr015@gmail.com>
2026-09-08 15:32:22 -0600

    algo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 function App() {
-return (
-<div>
-<h1>Hack Kit</h1>
-</div>
-)
+  return (
+    <div>
+      <h1>Hack Kit</h1>
+    </div>
+  )
 }
-export default App
+
+function Example() {
+  const name = "Isaac"
+  const isLogged = true
+  return (
+    <>
+    <h1 className="title">Hello, {name}</h1>
+    <p>{1 + 1}</p>
+    <p>{name.toUpperCase()}</p>
+    <br />
+    <img src="/vite.svg" alt="logo"/>
+    <button onClick={()=> alert('hi')} tabIndex={0}>Click</button>
+    <div style={{backgroundColor: 'lightblue', fontSize:20}}>boxed</div>
+    </>
+  )
+}
+
+export default Example
