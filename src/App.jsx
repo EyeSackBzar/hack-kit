eb6531ca5edf2ae9f38e7157daf96c45cabed772
isaac b r <isaacbr015@gmail.com>
2026-09-07 22:42:35 -0600

    a few wthings to test the branch thing

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get not so hard l</h1>
+          <h1>this is clearly just a test lol</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
