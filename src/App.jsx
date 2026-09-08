db0d2b048ff4ce29aaa4a9acd92946310c7fa6e6
isaac b r <isaacbr015@gmail.com>
2026-09-07 22:50:58 -0600

    testing merging twice lol

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>this is clearly just a test lol</h1>
+          <h1>something about test 2 lol</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
